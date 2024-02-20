47f839d76e2368a4c8d420c30a89612ed6a56623
metal <rex2644@outlook.com>
2024-02-20 10:25:45 +0100

    Nettoyage fichiers de base

--- epsi-exercice-sfc-2023/src/App.vue
+++ epsi-exercice-sfc-2023/src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 import BarreRecherche from "@/components/BarreRecherche.vue";
 </script>
 
@@ -9,13 +8,7 @@ import BarreRecherche from "@/components/BarreRecherche.vue";
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-<!--      <HelloWorld msg="You did it!" />-->
       <barre-recherche></barre-recherche>
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
     </div>
   </header>
 
