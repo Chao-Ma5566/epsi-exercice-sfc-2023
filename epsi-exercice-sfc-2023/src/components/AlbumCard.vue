f118c38fa36d6b9b8124a830fbb28209e3adaa1e
Chao-Ma5566 <mjc1378098@gmail.com>
2024-02-20 11:36:44 +0100

    add album card

---
+++ epsi-exercice-sfc-2023/src/components/AlbumCard.vue
@@ -0,0 +1,40 @@
+<template>
+    <section>
+      <h3>{{ albumInfo.title }}</h3>
+      <img :src=albumInfo.cover alt="cover album">
+      <p>Nombres musiques: {{ albumInfo.nb_tracks }}</p>
+    </section>
+</template>
+
+<script setup>
+defineProps({
+  albumInfo: {
+    required: true
+  }
+})
+</script>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
--- epsi-exercice-sfc-2023/src/views/AlbumView.vue
+++ epsi-exercice-sfc-2023/src/views/AlbumView.vue
@@ -3,7 +3,8 @@
       <div>
           <h2>Albums de Daft Punk</h2>
           <ul>
-              <li v-for="album in daftPunkAlbums" :key="album.id">
+              <li v-for="album in daftPunkAlbums" :key="album.id" v-cloak>
+                  <AlbumCard :albumInfo="album"></AlbumCard>
                   <h3>{{ album.title }}</h3>
                   <img :src=album.cover alt="cover album">
                   <p>Nombres musiques: {{ album.nb_tracks }}</p>
@@ -15,7 +16,7 @@
   
   <script>
   import axios from 'axios';
-  
+  import AlbumCard from "../components/AlbumCard.vue"
   const apiURL = "https://api.deezer.com/search/album?q=artist:\"Daft Punk\"";
   
   export default {
@@ -32,6 +33,7 @@
               try {
                   const response = await axios.get(apiURL);
                   this.daftPunkAlbums = response.data.data;
+                  console.log(this.daftPunkAlbums)
               } catch (error) {
                   console.error("Une erreur s'est produite lors de la récupération des albums de Daft Punk :", error);
                   this.daftPunkAlbums = [];
