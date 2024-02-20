0cb1c54cb6278ebced83529dda4cfa0a300d2698
Quentin COUET <qcouet49@gmail.com>
2024-02-20 10:36:10 +0100

    recupération des données album

--- epsi-exercice-sfc-2023/src/App.vue
+++ epsi-exercice-sfc-2023/src/App.vue
@@ -5,10 +5,10 @@ import BarreRecherche from "@/components/BarreRecherche.vue";
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
 
     <div class="wrapper">
-      <barre-recherche></barre-recherche>
+      <!-- <barre-recherche></barre-recherche> -->
     </div>
   </header>
 
--- epsi-exercice-sfc-2023/src/router/index.js
+++ epsi-exercice-sfc-2023/src/router/index.js
@@ -1,9 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-  ]
+    history: createWebHistory(
+        import.meta.env.BASE_URL),
+    routes: [{
+        path: '/detail',
+        name: 'detail',
+        component: () =>
+            import ('../views/PageDetailAlbum.vue')
+    }]
 })
 
 export default router
