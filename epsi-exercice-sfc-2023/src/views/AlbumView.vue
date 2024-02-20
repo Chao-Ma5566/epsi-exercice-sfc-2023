a760186546c025acd5018e93aef47d3c0db4f4a2
CharlyRc <ricoul.charly@orange.fr>
2024-02-20 10:17:54 +0100

    mise en place requete api albums + route

---
+++ epsi-exercice-sfc-2023/src/views/AlbumView.vue
@@ -0,0 +1,34 @@
+<template>
+    <main>
+    
+    </main>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  const apiURL = "https://api.deezer.com/search/album?q=artist:\"Daft Punk\"";
+  
+  export default {
+      data() {
+          return {
+              daftPunkAlbums: []
+          };
+      },
+      created() {
+          this.fetchDaftPunkAlbums();
+      },
+      methods: {
+          async fetchDaftPunkAlbums() {
+              try {
+                  const response = await axios.get(apiURL);
+                  this.daftPunkAlbums = response.data.data;
+              } catch (error) {
+                  console.error("Une erreur s'est produite lors de la récupération des albums de Daft Punk :", error);
+                  this.daftPunkAlbums = [];
+              }
+          }
+      },
+  };
+  </script>
+  
